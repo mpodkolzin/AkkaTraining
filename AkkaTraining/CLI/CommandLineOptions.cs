55e4cb699321fbb7e472a79d2b90279d541b0f35
Max Underwellman <mpodkolzin87@gmail.com>
2018-02-07 14:15:47 -0800

    First commit

---
+++ AkkaTraining/CLI/CommandLineOptions.cs
@@ -0,0 +1,44 @@
+﻿
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace AkkaTraining.CLI
+{
+    class CommandLineOptions
+    {
+
+        [Option('t', "topic", Required = true,
+            HelpText = "Kafka topic")]
+        public string Topic { get; set; }
+
+    }
+
+    [Verb("start", HelpText = "Add file contents to the index.")]
+    class StartOptions
+    {
+        [Option("id", Default =false, Required = true,
+            HelpText = "Job Id")]
+        public string JobId { get; set; }
+    }
+
+    [Verb("stop", HelpText = "Add file contents to the index.")]
+    class StopOptions
+    {
+        [Option("id", Default =false, Required = true,
+            HelpText = "Job Id")]
+        public string JobId { get; set; }
+    }
+
+    [Verb("status", HelpText = "Add file contents to the index.")]
+    class StatusOptions
+    {
+        [Option("id", Default =false, Required = true,
+            HelpText = "Job Id")]
+        public string JobId { get; set; }
+    }
+}
---
+++ AkkaTraining/Program.cs
@@ -0,0 +1,63 @@
+﻿using AkkaTraining.CLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace AkkaTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                var input = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Input cannot be empty;");
+                    continue;
+                }
+
+                parseCommandLineArgs(input.Split(' '));
+
+            }
+
+
+        }
+
+        private static void parseCommandLineArgs(IEnumerable<string> args)
+        {
+            if(args == null || !args.Any())
+            {
+            return;
+            }
+            var res = Parser.Default.ParseArguments<StartOptions, StopOptions, StatusOptions>(args)
+                .MapResult(
+                    (StartOptions opts) => StartJob(opts.JobId),
+                    (StopOptions opts) => StopJob(opts.JobId),
+                    (StatusOptions opts) => GetJobStatus(opts.JobId),
+                    e => false);
+        }
+
+
+        public static bool StartJob(string jobId)
+        {
+            return false;
+
+        }
+        public static bool StopJob(string jobId)
+        {
+            return false;
+
+        }
+        public static bool GetJobStatus(string jobId)
+        {
+            return false;
+
+        }
+    }
+}
