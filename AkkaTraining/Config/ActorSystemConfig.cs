ea5e06c8d455da56ba27e629afacbe49ce22902f
Max Underwellman <mpodkolzin87@gmail.com>
2018-02-07 14:42:35 -0800

    (add) Added config file

---
+++ AkkaTraining/Config/ActorSystemConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Configuration;
+
+namespace AkkaTraining.Config
+{
+    public static class ActorSystemConfig
+    {
+        public static Akka.Configuration.Config GetActorSystemConfig()
+        {
+
+            string configString = @"
+                     akka {
+                        loglevel = DEBUG
+                        #loggers = [""Akka.Logger.NLog.NLogLogger, Akka.Logger.NLog""]
+                        log-remote-lifecycle-events = INFO
+                     }";
+
+            return ConfigurationFactory.ParseString(configString);
+        }
+    }
+}
