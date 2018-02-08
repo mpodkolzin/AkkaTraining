0fe662883d8bc3cf15a62b7b958f6af19ce65480
Max Underwellman <mpodkolzin87@gmail.com>
2018-02-08 10:58:48 -0800

    (add) Added Unit tests project

---
+++ AkkaTrainingUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Akka.TestKit.VsTest;
+
+namespace AkkaTrainingUnitTests
+{
+    [TestClass]
+    public class UnitTest1 : TestKit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
