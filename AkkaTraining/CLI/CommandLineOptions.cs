78c534403ab574b1bbfd1544968fc1a20cff6af9
Max Underwellman <mpodkolzin87@gmail.com>
2018-02-07 17:44:12 -0800

    (edit) cleaned up CLI

--- AkkaTraining/CLI/CommandLineOptions.cs
+++ AkkaTraining/CLI/CommandLineOptions.cs
@@ -9,15 +9,6 @@ using CommandLine;
 
 namespace AkkaTraining.CLI
 {
-    class CommandLineOptions
-    {
-
-        [Option('t', "topic", Required = true,
-            HelpText = "Kafka topic")]
-        public string Topic { get; set; }
-
-    }
-
     [Verb("start", HelpText = "Add file contents to the index.")]
     class StartOptions
     {
@@ -37,7 +28,7 @@ namespace AkkaTraining.CLI
     [Verb("status", HelpText = "Add file contents to the index.")]
     class StatusOptions
     {
-        [Option("id", Default =false, Required = true,
+        [Option("id", Required = false,
             HelpText = "Job Id")]
         public string JobId { get; set; }
     }
