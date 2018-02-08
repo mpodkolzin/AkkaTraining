4df70e811958279a769520ff67a6418606a0bfc2
Max Underwellman <mpodkolzin87@gmail.com>
2018-02-08 13:55:11 -0800

    Revert "Test run"

--- AkkaTraining/Config/ActorSystemConfig.cs
+++ AkkaTraining/Config/ActorSystemConfig.cs
@@ -15,43 +15,9 @@ namespace AkkaTraining.Config
             string configString = @"
                      akka {
                         loglevel = DEBUG
-                        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
                         #loggers = [""Akka.Logger.NLog.NLogLogger, Akka.Logger.NLog""]
-                        actor {
-                            deployment {
-                                /JobCoordinator/JobsBroadcaster {
-                                   router = broadcast-group
-                                   routees.paths = [""/user/JobCoordinator/JobActor*""]
-                                   cluster {
-                                      enabled = on
-                                      allow-local-routees = on
-                                      use-role = worker
-                                   }
-                                }
-                                /JobCoordinator/AdHocJobsRouter {
-                                   router = consistent-hashing-pool
-                                   nr-of-instances = 5
-                                   cluster {
-                                      enabled = on
-                                      allow-local-routees = on
-                                      use-role = worker
-                                   }
-                                }
-                            }
-                        }
-                      remote {
-                        dot-netty.tcp {
-                          transport-protocol = tcp
-                          port = 0
-                          hostname = ""mpolenovo""
-                        }
-                      }
-                      cluster {
-                        seed-nodes = [""mpodlenovo""]
-                        roles = [worker]
-                      }
-
-                    }";
+                        log-remote-lifecycle-events = INFO
+                     }";
 
             return ConfigurationFactory.ParseString(configString);
         }
